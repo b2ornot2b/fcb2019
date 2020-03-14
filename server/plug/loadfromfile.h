c1ed2e38301272735498b35f501a7f83dff67f77
Blinston S. Fernandes <b2ornot2b@gmail.com>
2020-03-14 17:24:01 +0530

    added plug

---
+++ server/plug/loadfromfile.h
@@ -0,0 +1,32 @@
+#ifndef LOADFROMFILE_H
+#define LOADFROMFILE_H
+
+#include <QTextEdit>
+#include <QFile>
+#include <QXmlStreamReader>
+#include "data_structs.h"
+#include "effects_enum.h"
+
+class LoadFromFile
+{
+public:
+    LoadFromFile(QFile*, QString*, struct amp_settings*, struct fx_pedal_settings[4]);
+    LoadFromFile(const LoadFromFile& other);
+    ~LoadFromFile();
+    
+    LoadFromFile& operator=(const LoadFromFile& other);
+    
+    void loadfile();
+
+private:
+    QString *name;
+    struct amp_settings *amplifier_settings;
+    struct fx_pedal_settings *fx_settings;
+    QXmlStreamReader *xml;
+
+    void parseAmp();
+    void parseFX();
+    void parseFUSE();
+};
+
+#endif // LOADFROMFILE_H
